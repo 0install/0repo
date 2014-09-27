9d05539798b5a2e723e0eddde31523175ea275ac
Thomas Leonard <talex5@gmail.com>
2014-09-27 11:52:26 +0100

    Fixed handling of xml:lang attribute
    We generated a binding xmlns:ns="http://www.w3.org/XML/1998/namespace",
    which is illegal.
    
    Reported by Bastian Eicher.

--- repo/namespace.py
+++ repo/namespace.py
@@ -2,7 +2,7 @@
 # See the README file for details, or visit http://0install.net.
 
 from zeroinstall.injector.namespaces import XMLNS_IFACE
-from xml.dom import Node, XMLNS_NAMESPACE
+from xml.dom import Node, XMLNS_NAMESPACE, XML_NAMESPACE
 
 class Namespace(object):
 	def __init__(self):
@@ -17,6 +17,8 @@ class Namespace(object):
 		"""
 		if namespace == XMLNS_IFACE:
 			return None
+		if namespace == XML_NAMESPACE:
+			return "xml"
 		existing_prefix = self.namespace_prefixes.get(namespace, None)
 		if existing_prefix:
 			return existing_prefix
@@ -39,7 +41,8 @@ class Namespace(object):
 		else:
 			prefix = self.register_namespace(uri)
 			element.setAttributeNS(uri, '%s:%s' % (prefix, name), value)
-			element.ownerDocument.documentElement.setAttributeNS(XMLNS_NAMESPACE, 'xmlns:' + prefix, uri)
+			if uri != XML_NAMESPACE:
+				element.ownerDocument.documentElement.setAttributeNS(XMLNS_NAMESPACE, 'xmlns:' + prefix, uri)
 
 	def import_node(self, target_doc, source_node):
 		"""Import a node for a new document, fixing up namespace prefixes as we go."""
@@ -57,7 +60,8 @@ class Namespace(object):
 					elem.removeAttributeNS(uri, name)
 				elif uri:
 					new_prefix = self.register_namespace(uri)
-					target_root.setAttributeNS(XMLNS_NAMESPACE, 'xmlns:' + new_prefix, uri)
+					if uri != XML_NAMESPACE:
+						target_root.setAttributeNS(XMLNS_NAMESPACE, 'xmlns:' + new_prefix, uri)
 					elem.removeAttributeNS(uri, name)
 					elem.setAttributeNS(uri, '%s:%s' % (new_prefix, name), value)
 
