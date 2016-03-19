f56c2d1ac6644d45ed33749c786207eef42c26a6
Thomas Leonard <talex5@gmail.com>
2016-03-19 18:21:01 +0000

    Travis: automatically trust signing keys

--- .travis-install.sh
+++ .travis-install.sh
@@ -4,3 +4,15 @@ cd 0install-linux-x86_64-2.10
 ./install.sh home
 export PATH=$HOME/bin:$PATH
 0install add 0test http://0install.net/2008/interfaces/0test.xml
+cat > ~/.config/0install.net/injector/trustdb.xml <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<trusted-keys xmlns="http://zero-install.sourceforge.net/2007/injector/trust">
+  <key fingerprint="DA9825AECAD089757CDABD8E07133F96CA74D8BA">
+    <domain value="0install.net"/>
+    <domain value="repo.roscidus.com"/>
+  </key>
+  <key fingerprint="AC9B973549D819AE22BCD08D22EA111A7E4242A4">
+    <domain value="repo.roscidus.com"/>
+  </key>
+</trusted-keys>
+EOF
